bf84aa7029ea1fe4b17d6d8b1f1d87b54c350411
Louis Toro <louistororosario@gmail.com>
2024-01-22 10:55:47 -0400

    program that prints the number of and the list of its arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,5 +10,7 @@ elif argc == 2:
     print("1: {}".format(str(sys.argv[1])))
 else:
     print("{} arguments:".format(argc - 1))
-    for my_arg in range(1, argc):
+    my_arg = 1
+    while my_arg < argc:
         print("{}: {}".format(my_arg, str(sys.argv[my_arg])))
+        my_arg += 1
