8a283ed9d8b173d581671cd988dbf354086063e1
Louis Toro <louistororosario@gmail.com>
2024-01-22 21:11:36 -0400

    program that prints all the names defined by the compiled module hidden_4.pyc

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     my_list = dir(hidden_4)
     max_len = len(my_list)
     for i in range(0, max_len):
-        string = my_list[_]
+        string = my_list[i]
         if string[:2] != "__" != string[-2:]:
             print(string)
