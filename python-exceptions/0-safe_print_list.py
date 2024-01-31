7ff13b97439118338eac2e84c768e6d2d2607476
Louis Toro <louistororosario@gmail.com>
2024-01-30 21:45:10 -0400

    function that prints x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    i = 0
+
+    while i < x:
+        try:
+            print(f"{my_list[i]}", end='')
+            count += 1
+        except IndexError:
+            pass
+        i += 1
+
+    print()
+    return count
+    
