46b82855eabf7923716dfae9acfb82b9b8ec7ec1
Louis Toro <louistororosario@gmail.com>
2024-01-31 17:28:43 -0400

    2 for in my print

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -52,6 +52,8 @@ class Square:
         if self.__size is 0:
             print()
         else:
+            for _ in range(self.__position[1]):
+                print()
             for _ in range(self.__size):
                 print(" " * self.__position[0], end='')
                 print(self.__size * '#')
