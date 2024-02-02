acd54aeb691f028d5c3ede33a51e6477969f8686
Louis Toro <louistororosario@gmail.com>
2024-02-01 22:22:19 -0400

    function that prints a square with the character #

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,11 +9,15 @@ Author: Alfre, Antonio, Louis, Date: Feb 01 2024
 def print_square(size):
     """Description: prints a square with the character #
     - size is the size length of the square"""
+    
     if type(size) is not int:
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
+
     if type(size) is float and type(size) < 0:
         raise TypeError("size must be an integer")
+
     for _ in range(size):
         print('#' * size)
