d0b523aa61f288d1cefc56c2d60ae7300cdbc7f3
Louis Toro <louistororosario@gmail.com>
2024-02-02 16:27:36 -0400

    update

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,40 +1,34 @@
 #!/usr/bin/python3
-"""
-Module Name: 2-matrix_divided
-Description: Divides all elements of a matrix
-Authors: Lusanco, Ltoro9, Antoniofdjs Date: Feb 01 2024
+""" matrix_divided divides the given matrix
+by the parameter "div", and returns the divided matrix
 """
 
 
 def matrix_divided(matrix, div):
-    """Description: Divides all elements of a matrix
-    - matrix (list): 2d array.
-    - div (int or float): Number to divide by."""
-
-    mtx = "matrix must be a matrix (list of lists) of integers/floats"
-    siz = "Each row of the matrix must have the same size"
-
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
+    """ Divides all elements of a matrix by "div"
+    checks if the entire list is int/float
+    checks if each list in the matrix are the same size
+    checks if "div" is an int/float or is 0
+    """
+    mes0 = "matrix must be a matrix (list of lists) of integers/floats"
+    mes1 = "Each row of the matrix must have the same size"
+    res_matrix = []
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
-    temp = len(matrix[0])
-    for row in matrix:
-        if not isinstance(row, list):
-            raise TypeError(mtx)
-        if temp != len(row):
-            raise TypeError(siz)
-        temp = len(row)
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
 
-    new_matrix = [row[:] for row in matrix]
+    for lists in matrix:
+        if len(lists) != len(matrix[0]):
+            raise TypeError(mes1)
+        inner_list = []
+        for items in lists:
+            if not isinstance(items, (int, float)):
+                raise TypeError(mes0)
+            else:
+                inner_list.append(round(items / div, 2))
+        res_matrix.append(inner_list)
 
-    for row in range(len(matrix)):
-        for column in range(len(matrix[row])):
-            if not isinstance(matrix[row][column], (int, float)):
-                raise TypeError(mtx)
-            result = round(matrix[row][column] / div, 2)
-            new_matrix[row][column] = result
-
-    return new_matrix
+    return res_matrix
