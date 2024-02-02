ce5f48c93a4eae34de52b3a21c5b7f13fdc5ce47
Louis Toro <louistororosario@gmail.com>
2024-02-01 23:35:51 -0400

    prints a text with 2 new lines after each'., ? and :'

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+Module Name: 5-text_indentation.py
+Description: prints a text with 2 new lines after each'., ? and :'
+Author: Alfre, Antonio, Louis, Date: Feb 01 2024
+"""
 def text_indentation(text):
+    """Description: prints a text with 2 new lines after each'., ? and :'
+    - text is a string"""
+    err = "unsupported operand type(s) for +: 'NoneType' and 'int'"
+    if text is None:
+        raise TypeError(err)
     if type(text) is not str:
         raise TypeError("text must be a string")
     c = 0
@@ -10,4 +20,3 @@ def text_indentation(text):
             if c + 1 < len(text) and text[c + 1] == ' ':
                 c += 1
         c += 1
-
