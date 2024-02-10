00e34299a6be701423a9a62b4d2a6a567838e0ad
Louis Toro <louistororosario@gmail.com>
2024-02-10 10:31:47 -0400

    fix pycodestyle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -14,6 +14,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """comment"""
 
@@ -21,7 +22,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
         self.__width = width
         self.integer_validator("width", width)
-        self.integer_validator("height",height)
+        self.integer_validator("height", height)
 
     def area(self):
         return self.__height * self.__width
