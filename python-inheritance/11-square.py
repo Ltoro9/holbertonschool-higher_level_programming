6f4d20cf7b9c6ba09046cd015ca87962d948aa7f
Louis Toro <louistororosario@gmail.com>
2024-02-10 11:31:09 -0400

    Square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""comment"""
+
+
+class BaseGeometry:
+    """comment"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """comment"""
+
+    def __init__(self, width, height):
+        self.__height = height
+        self.__width = width
+        self.integer_validator("width", width)
+        self.integer_validator("height",height)
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+class Square(Rectangle):
+    """comment"""
+    def __init__(self, size):
+        BaseGeometry().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+    
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
