3248770d0b78cd0fa4e272008dcd5883b0f60eb6
Louis Toro <louistororosario@gmail.com>
2024-02-10 11:49:54 -0400

    class Rectangle that inherits from BaseGeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,24 +2,13 @@
 """comment"""
 
 
-class BaseGeometry:
-    """comment"""
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """comment"""
 
     def __init__(self, width, height):
-        super().__init__()
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
