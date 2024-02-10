6a88e17fbdcec8c16a10ed38e43996378a737d43
Louis Toro <louistororosario@gmail.com>
2024-02-10 10:38:05 -0400

    super() added

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -19,6 +19,7 @@ class Rectangle(BaseGeometry):
     """comment"""
 
     def __init__(self, width, height):
+        super().__init__()
         self.integer_validator("width", width)
         self.integer_validator("height",height)
         self.__width = width
