6952d54ec2394b2ec8a4ec82aa9d69eee0f51b3d
Louis Toro <louistororosario@gmail.com>
2024-02-10 11:36:24 -0400

    Square

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -14,6 +14,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """comment"""
 
@@ -21,13 +22,14 @@ class Rectangle(BaseGeometry):
         self.__height = height
         self.__width = width
         self.integer_validator("width", width)
-        self.integer_validator("height",height)
+        self.integer_validator("height", height)
 
     def area(self):
         return self.__height * self.__width
 
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
+
 
 class Square(Rectangle):
     """comment"""
