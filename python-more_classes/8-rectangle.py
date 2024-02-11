48960750c77ea533f97d7d242f108f27f6372412
Louis Toro <louistororosario@gmail.com>
2024-02-11 18:12:06 -0400

    rectangle

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -9,28 +9,31 @@ class Rectangle:
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if type(height) is not int:
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
+
         Rectangle.number_of_instances += 1
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if rect_1 is not Rectangle:
-            TypeError("rect_1 must be an instance of Rectangle")
-        if rect_2 is not Rectangle:
-            TypeError("rect_2 must be an instance of Rectangle")
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
         if rect_1.area() >= rect_2.area():
             return rect_1
-        else:
-            return rect_2
+
+        return rect_2
 
     @property
     def width(self):
@@ -38,7 +41,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -50,32 +53,30 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    def area(self):
-        return self.__height * self.__width
-
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__height + self.__width)*2
+        return (self.__width + self.__height) * 2
+
+    def area(self):
+        return self.__height * self.__width
 
     def __str__(self):
-        a = ""
-        if self.__width == 0 or self.__height == 0:
-            return a
-        for _ in range(self.__height):
-            a += str(self.print_symbol) * self.__width
-            a += '\n'
-        return a.strip()
-    
+        my_str = ""
+        for _ in range(0, self.__height):
+            my_str += str(self.print_symbol) * self.__width
+            my_str += "\n"
+        return my_str.strip()
+
     def __repr__(self):
-        return (f"Rectangle ({self.__width}, {self.__height})")
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
-        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
