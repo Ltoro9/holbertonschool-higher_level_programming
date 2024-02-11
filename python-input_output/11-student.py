c9a2179e8f80a46a42e80dfe99fb54ee7ba75546
Louis Toro <louistororosario@gmail.com>
2024-02-11 17:17:54 -0400

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""comment"""
+
+
+class Student:
+    """comment"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            # a is attribute
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
