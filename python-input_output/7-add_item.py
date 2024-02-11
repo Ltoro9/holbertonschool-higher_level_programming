573f28f8b27df55c2698f63b21db49f5616c44ae
Louis Toro <louistororosario@gmail.com>
2024-02-11 11:22:41 -0400

    script that adds all arguments to a Python list, and then save them to a file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""comment"""
+
+
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+def add_item(args):
+    """comment"""
+
+    my_list = []
+
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
+
+    my_list.extend(args)
+    save_to_json_file(my_list, "add_item.json")
