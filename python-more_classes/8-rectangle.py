306367a7790489143a2c5a7103c8c583002a541b
Louis Toro <louistororosario@gmail.com>
2024-02-11 18:05:12 -0400

    rectangle

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -21,6 +21,17 @@ class Rectangle:
         self.__width = width
         Rectangle.number_of_instances += 1
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if rect_1 is not Rectangle:
+            TypeError("rect_1 must be an instance of Rectangle")
+        if rect_2 is not Rectangle:
+            TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+
     @property
     def width(self):
         return self.__width
@@ -68,14 +79,3 @@ class Rectangle:
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        if rect_1 is not Rectangle:
-            TypeError("rect_1 must be an instance of Rectangle")
-        if rect_2 is not Rectangle:
-            TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        else:
-            return rect_2
