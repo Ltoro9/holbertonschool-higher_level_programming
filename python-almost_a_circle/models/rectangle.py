a51869f32ffe60f8522fe07d7a261ccb5103847f
Louis Toro <louistororosario@gmail.com>
2024-02-13 13:33:08 -0400

    updating the public method that assigns a key/value argument to attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -98,15 +98,18 @@ class Rectangle(Base):
         h = self.__height
         return f"[Rectangle] ({self.id}) {x}/{y} - {w}/{h}"
 
-    def update(self, *args):
-        """comment"""
-        if len(args) == 1:
-            self.id = args[0]
-        if len(args) == 2:
-            self.__width = args[1]
-        if len(args) == 3:
-            self.__height = args[2]
-        if len(args) == 4:
-            self.__x = args[3]
-        if len(args) == 5:
-            self.__y = args[4]
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) == 1:
+                self.id = args[0]
+            if len(args) == 2:
+                self.__width = args[1]
+            if len(args) == 3:
+                self.__height = args[2]
+            if len(args) == 4:
+                self.__x = args[3]
+            if len(args) == 5:
+                self.__y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
