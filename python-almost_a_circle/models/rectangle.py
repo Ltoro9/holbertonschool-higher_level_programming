b9b30092b513397b7778a5c024126d63acf80e8c
Louis Toro <louistororosario@gmail.com>
2024-02-13 10:52:20 -0400

    update the argument to each attribute

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,15 @@ class Rectangle(Base):
         w = self.__width
         h = self.__height
         return f"[Rectangle] ({self.id}) {x}/{y} - {w}/{h}"
+
+    def update(self, *args):
+        if len(args) == 1:
+            self.id = args[0]
+        if len(args) == 2:
+            self.__width = args[1]
+        if len(args) == 3:
+            self.__height = args[2]
+        if len(args) == 4:
+            self.__x = args[3]
+        if len(args) == 5:
+            self.__y = args[4]
