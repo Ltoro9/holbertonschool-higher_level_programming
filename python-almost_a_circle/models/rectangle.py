32691246aaa1564fddfb4a3c9dc48eba67f8da09
Louis Toro <louistororosario@gmail.com>
2024-02-12 20:29:29 -0400

    area value of the Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,6 @@ from models.base import Base
 class Rectangle(Base):
     """comment"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width <= 0:
@@ -25,6 +24,7 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if y < 0:
             raise ValueError("y must be >= 0")
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
