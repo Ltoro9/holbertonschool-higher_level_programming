a75d0795b139e8da5433e103165b5f96709311b9
Louis Toro <louistororosario@gmail.com>
2024-02-13 23:36:42 -0400

    JSON string to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -27,3 +27,14 @@ class Base:
 
     def __repr__(self):
         return str(self.to_json_string())
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """comment"""
+        if list_objs is None:
+            file.write("[]")
+        filename = str(cls.__name__) + ".json"
+        json_list = [obj.to_dictionary() for obj in list_objs]
+        json_str = cls.to_json_string(json_list)
+        with open(filename, "w") as file:
+            file.write(json_str)
