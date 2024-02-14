b248826b7e3aa4c066cbb380d14f726f1f911f97
Louis Toro <louistororosario@gmail.com>
2024-02-13 20:29:28 -0400

    fix pycodestyle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,17 +87,10 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """comment"""
+        my_list = ['id', 'width', 'height', 'x', 'y']
         if args:
-            if len(args) == 1:
-                self.id = args[0]
-            if len(args) == 2:
-                self.__width = args[1]
-            if len(args) == 3:
-                self.__height = args[2]
-            if len(args) == 4:
-                self.__x = args[3]
-            if len(args) == 5:
-                self.__y = args[4]
+            for name, value in zip(my_list, args):
+                setattr(name, value)
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
