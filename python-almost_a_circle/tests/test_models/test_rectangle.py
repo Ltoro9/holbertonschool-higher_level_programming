97829aa9b96e0b6dba49d9190684f3d3e3c858cc
Louis Toro <louistororosario@gmail.com>
2024-02-16 09:26:57 -0400

    unittest rectangle

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""comment"""
+
+
+import unittest
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+
+    def test_rectangle_creation(self):
+        r = Rectangle(1, 2)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+
+if __name__ == '__main__':
+    unittest.main()
