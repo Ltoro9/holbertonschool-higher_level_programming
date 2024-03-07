2a9b71d7bff940402bf434c1c0ddf55d141e34eb
Louis Toro <louistororosario@gmail.com>
2024-03-07 12:40:25 -0400

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
--- script that lists all rows of the table first_table
-SELECT *
-FROM first_table;
+-- script that prints the following description of the table first_table
+SHOW CREATE TABLE first_table;
