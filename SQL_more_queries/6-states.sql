22aa16788d0e15841fc0e9417f35e519b193d6ea
Louis Toro <louistororosario@gmail.com>
2024-03-09 21:44:31 -0400

    States table

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
+-- script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
