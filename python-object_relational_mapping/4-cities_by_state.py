93871ab098b48f1015519fea1ef9da4909a47efc
Louis Toro <louistororosario@gmail.com>
2024-03-11 20:50:05 -0400

    Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -23,7 +23,7 @@ if __name__ == "__main__":
                     FROM cities\
                     INNER JOIN states ON cities.state_id = states.id\
                     ORDER BY cities.id ASC")
-    
+
     rows = cursor.fetchall()
 
     for row in rows:
